ad9a4503395c06aa8411faacd119fb554831684d
small-pony <2262713487@qq.com>
2020-08-29 15:48:59 +0800

    style(main): using clang-format to format the code
    Using clang-format to format the code.

--- src/L.cpp
+++ src/L.cpp
@@ -1,14 +1,13 @@
 #include <L.hpp>
 #include <L/lexer.hpp>
-int main(int argc, char const *argv[])
-{
-    using namespace std;
-    using namespace L;
-    using namespace L::Base;
-    ifstream fin(argv[1]);
-    Lexer lexer(fin);
-    for (auto &&i : lexer.get()){
-        cout << i << ",";
-    }
-    return 0;
+int main(int argc, char const* argv[]) {
+	using namespace std;
+	using namespace L;
+	using namespace L::Base;
+	ifstream fin(argv[1]);
+	Lexer lexer(fin);
+	for (auto&& i : lexer.get()) {
+		cout << i << ",";
+	}
+	return 0;
 }
